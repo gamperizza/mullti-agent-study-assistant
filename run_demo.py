3afee62e247fe1e1f93b4afeb140d12280255eab
gamperizza <eggy2911@gmail.com>
2025-12-19 20:54:13 +0300

    Add files

---
+++ run_demo.py
@@ -0,0 +1,8 @@
+from src.agents import MultiAgentSystem
+from src.utils import interactive_demo
+
+"""Interactive demo"""
+
+if __name__ == "__main__":
+    system = MultiAgentSystem()
+    interactive_demo(system)
